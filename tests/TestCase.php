76837efc9bfb3628c50b67e5909740ea3e7527b1
mingkkday <ming.shih@kkday.com>
2022-10-25 09:16:45 +0800

    article api test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,29 @@
 
 namespace Tests;
 
+use App\Models\User;
+use App\Models\Article;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+
+    public function createUser()
+    {
+        $this->user = User::factory()->create();
+    }
+
+    /**
+     * @uses TestCase::createUser()
+     */
+    public function createArticle()
+    {
+        $this->article  = Article::factory()->create([
+            'user_id' => $this->user->id,
+            'photo' => "photos/test.png",
+        ]);
+    }
 }
